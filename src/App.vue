0258fc68ca5984bbf22f0f626e4fb85248dbe583
Голубев Антон Евгеньевич <anton.golubev@becloud.by>
2021-11-26 12:04:39 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div></div>
+
+<!--  <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from-->
+<!--    <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>-->
+<!--  </div>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/styles/main.css";
 </style>
